5ef773665803f1d0908de645429ebfe2769406ae
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-09-18 17:31:27 +0300

    feat: add /flowsheets route

---
+++ client/apps/cms/src/routes/_app/route.tsx
@@ -0,0 +1,23 @@
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
+import { UserListQuery } from '@/features/user/queries.ts';
+import { AppShell } from '@/shared/components/new/app-shell.tsx';
+
+export const Route = createFileRoute('/_app')({
+  beforeLoad: async ({ context, location }) => {
+    if (!context.isAuthenticated) {
+      throw redirect({ to: '/login', search: { redirect: location.pathname } });
+    }
+  },
+  loader: ({ context: { queryClient } }) => {
+    queryClient.ensureQueryData(UserListQuery);
+  },
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <AppShell>
+      <Outlet />
+    </AppShell>
+  );
+}
