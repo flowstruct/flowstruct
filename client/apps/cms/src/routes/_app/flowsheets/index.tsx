952a1f2a02b5d5562c4cf78c54b46f673a3c8217
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-09-19 20:24:48 +0300

    feat: /flowsheet route

--- client/apps/cms/src/routes/_app/flowsheets/index.tsx
+++ client/apps/cms/src/routes/_app/flowsheets/index.tsx
@@ -1,6 +1,15 @@
-import { createFileRoute, stripSearchParams } from '@tanstack/react-router';
+import { createFileRoute, Link, stripSearchParams } from '@tanstack/react-router';
 import { getTableSearchSchema } from '@/shared/schemas.ts';
 import { DefaultSearchValues } from '@/utils/defaultSearchValues.ts';
+import {
+  Breadcrumb,
+  Breadcrumbs,
+  Header,
+  HeaderLeft,
+  HeaderRight,
+} from '@/shared/components/new/header.tsx';
+import { Layers2, Plus } from 'lucide-react';
+import { Button } from '@/shared/components/ui2/Button.tsx';
 
 export const Route = createFileRoute('/_app/flowsheets/')({
   component: RouteComponent,
@@ -8,8 +17,29 @@ export const Route = createFileRoute('/_app/flowsheets/')({
   search: {
     middlewares: [stripSearchParams(DefaultSearchValues())],
   },
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_app/flowsheets/"!</div>
+  return (
+    <div>
+      <Header>
+        <HeaderLeft>
+          <Breadcrumbs>
+            <Link to="/flowsheets" search={DefaultSearchValues()}>
+              <Breadcrumb base>
+                <Layers2 size={14} /> Flowsheets
+              </Breadcrumb>
+            </Link>
+          </Breadcrumbs>
+        </HeaderLeft>
+
+        <HeaderRight>
+          <Button variant="transparent">
+            <Plus size={16} />
+          </Button>
+        </HeaderRight>
+      </Header>
+      <div></div>
+    </div>
+  );
 }
